f4a3f66dae27973eda7dfd806f63b4f121ac1ddf
Nagehan Uyumaz <100793893+Nagehan1@users.noreply.github.com>
2022-10-13 00:20:57 +0100

    level100-200

--- script.js
+++ script.js
@@ -2,12 +2,52 @@
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
-  console.log(allEpisodes)
+
+  document.getElementById("searchInput")
+  .addEventListener("input",searchEpisode)
+   
+
+  
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+ const container = document.getElementById("episodes") 
+episodeList.forEach(episode => {
+  const card = document.createElement("div")
+  card.className ="card";
+  const title =document.createElement("h1")
+  title.className ="title";
+ title.innerText = `${episode.name}- ${episode.season}`
+ card.appendChild(title) 
+
+ const image =document.createElement("img") 
+ image.className ="image";
+ image.src = episode.image.medium
+ card.appendChild(image); 
+
+ const summary =document.createElement("p")
+ summary.className ="summary";
+ summary.innerHTML =episode.summary
+ card.appendChild(summary)
+ container.appendChild(card);
+
+ 
+});
+const count = document.getElementById("countDisplay")
+count.innerText =`displaying ${episodeList.length}/${getAllEpisodes().length} episode`
 }
+function searchEpisode(){
+  const searchInput =document.getElementById("searchInput").value
+  const allEpisodes = getAllEpisodes();
+  const filteredEpisode = allEpisodes.filter(episode=>{
+    return(episode.name.toLowerCase().includes(searchInput.toLowerCase()) 
+    || episode.summary.toLowerCase().includes(searchInput.toLowerCase()) )
+  })
+ const container = document.getElementById("episodes")
+ container.innerHTML = "";
+ makePageForEpisodes(filteredEpisode) 
+
+}
+
 
 window.onload = setup;
